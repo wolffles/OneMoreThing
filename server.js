b2ed71145cf41a5b78a8d453fbcd00b17fefa687
woffles <wolfgang.truong@gmail.com>
2019-04-07 15:28:55 -0700

    first commit

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', () => res.send('Hello'))
+app.get('/', () => res.send('Hello'));
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`));

0dacf7b8f0e721765c7df531fe992aaabacf8465
woffles <wolfgang.truong@gmail.com>
2019-04-13 13:58:03 -0700

    pushing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          {/* <Navbar /> */}
+          {/* <Route exact path='/' component={Landing}/> */}
+          {/* we put all other components in container because landing is the only component taht takes up the whole screen */}
+          <div className="container">
+          <h1>hello</h1>
+            {/* <Route exact path='/register'/>
+            <Route exact path='/login' /> */}
+          </div>
+          {/* <Footer /> */}
+        </div>
+      </BrowserRouter>
     );
   }
 }

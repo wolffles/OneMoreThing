5034ce5d0efe8604fad4b73a311434e9dc413403
woffles <wolfgang.truong@gmail.com>
2019-04-13 14:00:38 -0700

    pushing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
+// import Navbar from './components/layout/Navbar';
+// import Footer from './components/layout/Footer';
+// import Landing from './components/layout/Landing';
 
 import './App.css';
 
